c14660de044298d627e2ef1509241045ad3f9aef
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-06-01 15:25:37 +0200

    update sign up with validation

--- static/main.js
+++ static/main.js
@@ -13,3 +13,23 @@
 //     console.log($('#firstName').val());
 //     console.log($('#signUpForm').serializeArray());
 // }
+
+$("#signUpForm").submit(function (event) {
+
+    // check that the form fields are completed
+    $("form#signUpForm :input").each(function() {
+
+        if($(this).val() === "") {
+            console.log($(this));
+            if (!($(this).get(0).id === "submitSignUpButton")) {
+                let fields_alert = '<div class="alert alert-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Warning!</strong> Fill out all fields before submitting.</div>';
+                $("#fieldsMissingAlert").html(fields_alert);
+                event.preventDefault();
+                return false;
+            }
+
+        }
+    });
+
+
+});
