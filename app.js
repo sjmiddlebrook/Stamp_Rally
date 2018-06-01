f94630b25f6216943b27d83cd44836dbfe5134dc
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-06-01 13:40:59 +0200

    update sign up and add people

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 var express = require('express');
+const bodyParser = require("body-parser");
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var path = require('path');
 app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.set('view engine', 'handlebars');
 app.use('/static', express.static(__dirname + '/static'));
 app.set('port', 3000);
 
+
 app.get('/',function(req,res){
-    res.render('home');
+    res.render('landing');
 });
 
 app.get('/sign_up',function(req,res){
     res.render('sign_up');
 });
 
+app.post("/sign_up", function (req, res) {
+    console.log(req.body);
+    // TODO submit the req.body data to the User database
+    res.redirect("/home");
+});
+
+app.get('/home',function(req,res){
+    res.render('home');
+});
+
 app.get('/add_people',function(req,res){
     res.render('add_people');
+});
+
+
+app.post("/add_people", function (req, res) {
+    console.log(req.body);
+    // TODO submit the req.body data to the User database
+    res.redirect("/home");
 });
 
 app.use(function(req,res){
--- static/main.js
+++ static/main.js
@@ -1,21 +1,15 @@
-// set the onclick functionality for the button
-document.getElementById("font_color_button").onclick = change_text_color;
-
-function change_text_color() {
-    // classes that will change the color
-    var text_color_classes = ["text-success", "text-secondary", "text-primary", "text-warning", "text-info"];
-    // get random number from 0 to the length of the number of text colors
-    var random_num = Math.floor(Math.random() * text_color_classes.length);
-    // select all the elements on the public golf course page with text
-    var public_page_elements = document.getElementsByClassName("public_page");
-    // loop over each element
-    for (var i = 0; i < public_page_elements.length; i++) {
-        console.log(public_page_elements[i].classList.length);
-        // if a text color class has already been added then remove it
-        if (public_page_elements[i].classList.length > 2) {
-            public_page_elements[i].classList.remove(public_page_elements[i].classList.item(2));
-        }
-        // add the text color based on the random number
-        public_page_elements[i].classList.add(text_color_classes[random_num]);
-    }
-}
+// document.getElementById("submitSignUpButton").addEventListener("click", function(event){
+//     event.preventDefault();
+//     submit_sign_up();
+// });
+//
+// $("#signUpForm").submit(function( event ) {
+//     console.log($(this).serializeArray());
+//     event.preventDefault();
+// });
+//
+// function submit_sign_up() {
+//     console.log('submitting...');
+//     console.log($('#firstName').val());
+//     console.log($('#signUpForm').serializeArray());
+// }
