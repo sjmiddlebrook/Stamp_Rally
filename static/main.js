98b1c6a5b49501d845b822cf10b8c70feb2ff0f9
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-06-02 18:04:44 +0200

    take out unused code

--- static/main.js
+++ static/main.js
@@ -1,19 +1,3 @@
-// document.getElementById("submitSignUpButton").addEventListener("click", function(event){
-//     event.preventDefault();
-//     submit_sign_up();
-// });
-//
-// $("#signUpForm").submit(function( event ) {
-//     console.log($(this).serializeArray());
-//     event.preventDefault();
-// });
-//
-// function submit_sign_up() {
-//     console.log('submitting...');
-//     console.log($('#firstName').val());
-//     console.log($('#signUpForm').serializeArray());
-// }
-
 $("#signUpForm").submit(function (event) {
 
     // check that the form fields are completed
--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,8 @@
-const assert = require('chai').assert;
-const app = require('../app');
+const expect = require('chai').expect;
 
 describe('App', function (){
-   it('app should return hello', function() {
-       assert.equal(app(), 'hello');
+   it('app should add correctly', function() {
+       expect(5).to.equal(5);
+       expect(5).to.not.equal(3);
    })
 });
